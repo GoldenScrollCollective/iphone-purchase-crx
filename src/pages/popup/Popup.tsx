11455e8eeee617998b60f63bb157f06933e0c9de
developer <developer@gmail.com>
2024-09-14 02:21:26 -0300

    initial

---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StoreList } from '@pages/popup/components';
+
+export default function Popup(): JSX.Element {
+  return (
+    <div className="container">
+      <StoreList />
+    </div>
+  );
+}
